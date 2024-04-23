d2e09b2c0446c7192cbf1af670bc4e6e9c482b2e
Administrator <alonegentleone@gmail.com>
2024-04-23 17:20:34 +0800

    更改

--- src/main/java/com/language/learn/utils/MD5.java
+++ src/main/java/com/language/learn/utils/MD5.java
@@ -8,32 +8,6 @@ import java.security.NoSuchAlgorithmException;
 public final class MD5 {
     // use DigestUtils
     public static String encrypt(String strSrc) {
-        try {
-            char[] hexChars = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-            byte[] bytes = strSrc.getBytes();
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            md.update(bytes);
-            bytes = md.digest();
-            int j = bytes.length;
-            char[] chars = new char[j * 2];
-            int k = 0;
-
-            for (byte b : bytes) {
-                chars[k++] = hexChars[b >>> 4 & 15];
-                chars[k++] = hexChars[b & 15];
-            }
-
-            return new String(chars);
-        } catch (NoSuchAlgorithmException var9) {
-            var9.printStackTrace();
-            throw new RuntimeException("MD5加密出错！！+" + var9);
-        }
-    }
-
-    public static void main(String[] args) {
-        var a = DigestUtils.md5DigestAsHex("admin".getBytes());
-        var b = encrypt("admin");
-        assert a.equals(b);
-
+        return DigestUtils.md5DigestAsHex(strSrc.getBytes());
     }
 }
