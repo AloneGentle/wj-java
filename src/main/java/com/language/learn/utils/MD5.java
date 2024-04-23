6cf331b1af23d7d24365b7650585759a633058e7
Administrator <alonegentleone@gmail.com>
2024-04-23 17:22:16 +0800

    更改

--- src/main/java/com/language/learn/utils/MD5.java
+++ src/main/java/com/language/learn/utils/MD5.java
@@ -3,7 +3,6 @@ package com.language.learn.utils;
 import org.springframework.util.DigestUtils;
 
 public final class MD5 {
-    // use DigestUtils
     public static String encrypt(String strSrc) {
         return DigestUtils.md5DigestAsHex(strSrc.getBytes());
     }
